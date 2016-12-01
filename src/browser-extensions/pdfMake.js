c2bafc0508fb6b70273fe698ae3e04d292c2e00f
Stephen Saucier <stephen@saucier.co>
2016-12-01 14:20:55 -0500

    Added more clarity in _createDoc

--- src/browser-extensions/pdfMake.js
+++ src/browser-extensions/pdfMake.js
@@ -35,30 +35,6 @@ function isArray(obj){
 	return !!obj && Array === obj.constructor;
 }
 
-function fork (async_calls, shared_callback) {
-	var counter = async_calls.length;
-	var all_results = [];
-	function makeCallback (index) {
-		return function () {
-			counter --;
-			var results = [];
-			// we use the arguments object here because some callbacks 
-			// in Node pass in multiple arguments as result.
-			for (var i=0;i<arguments.length;i++) {
-				results[i] = arguments[i];
-			}
-			all_results[index] = results;
-			if (counter == 0) {
-				shared_callback(all_results);
-			}
-		}
-	}
-
-	for (var i=0;i<async_calls.length;i++) {
-		async_calls[i](makeCallback(i));
-	}
-}
-
 Document.prototype._createDoc = function(options, callback) {
 	var printer = new PdfPrinter(this.fonts);
 	printer.fs.bindFS(this.vfs);
@@ -79,6 +55,7 @@ Document.prototype._createDoc = function(options, callback) {
 
 			docs[i].on('data', function(chunk) {
 				chunks.push(chunk);
+				console.info(chunk);
 			});
 			docs[i].on('end', function() {
 				makePages[i] = docs[i]._pdfMakePages;
