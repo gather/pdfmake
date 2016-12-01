8852bf7d2a201fa33cc88b67d96874c884832f28
Stephen Saucier <stephen@saucier.co>
2016-12-01 15:23:52 -0500

    Set createDoc to concatenate content instead

--- src/browser-extensions/pdfMake.js
+++ src/browser-extensions/pdfMake.js
@@ -39,41 +39,25 @@ Document.prototype._createDoc = function(options, callback) {
 	var printer = new PdfPrinter(this.fonts);
 	printer.fs.bindFS(this.vfs);
 
-	var docs = [];
-	var makePages = [];
 	var chunks = [];
 	var result;
 	var docDefinition = this.docDefinition;
-	if (!isArray(docDefinition)) docDefinition = [docDefinition];
-
-	var asyncFunctions = [];
-	var i = 0;
-
-	function appendDoc(i) {
-		return new Bluebird(function (resolve, reject) {
-			docs[i] = printer.createPdfKitDocument(docDefinition[i], options);
-
-			docs[i].on('data', function(chunk) {
-				chunks.push(chunk);
-				console.info(chunk);
-			});
-			docs[i].on('end', function() {
-				makePages[i] = docs[i]._pdfMakePages;
-			});
-			return resolve(docs[i].end());
-		});
+	if (isArray(docDefinition)) {
+		docDefinition = docDefinition[0];
+		for (var i = 0; i < docDefinition.length; i++) {
+			docDefinition.content.push(this.docDefinition[i].content);
+		}
 	}
+	var doc = printer.createPdfKitDocument(docDefinition, options);
 
-	for (i; i < docDefinition.length; i++) {
-		asyncFunctions.push(appendDoc(i));
-	}
-	Bluebird.all(asyncFunctions)
-	.then(function () {
-		result = Buffer.concat(chunks);
-		console.info(makePages[0]);
-		callback(result, makePages[0]);
-		// callback(result);
+	doc.on('data', function(chunk) {
+		chunks.push(chunk);
 	});
+	doc.on('end', function() {
+		result = Buffer.concat(chunks);
+		callback(result, doc._pdfMakePages);
+	});
+	doc.end();
 };
 
 Document.prototype._getPages = function(options, cb){
